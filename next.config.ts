d42b67d739332d5eac2eea13560c6e7505fbca91
Martin Ng <martinng2002@gmail.com>
2026-09-16 09:42:26 -0700

    remove warning suppression

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: { optimizePackageImports: ['framer-motion'] },
-  ignoreWarnings: [
-    {
-      module: /node_modules\/jose\/dist\/webapi\/lib\/deflate\.js/,
-    },
-  ],
 };
 
 
