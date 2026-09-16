8ed9e3799a3bb66486dc14fc268a77960d42abf8
Martin Ng <martinng2002@gmail.com>
2026-09-16 09:38:34 -0700

    upgrade bun ver

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: { optimizePackageImports: ['framer-motion'] },
+  ignoreWarnings: [
+    {
+      module: /node_modules\/jose\/dist\/webapi\/lib\/deflate\.js/,
+    },
+  ],
 };
+
 
 export default nextConfig;
